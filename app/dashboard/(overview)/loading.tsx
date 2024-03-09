f0bc64871f236418fcd1d2ca2847bc521e99d484
Chouaib-Djerdi <djerdichouaib@gmail.com>
2024-03-09 15:30:51 +0100

    Streaming & Suspense

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,7 @@
+import DashboardSkeleton from '@/app/ui/skeletons';
+
+const loading = () => {
+  return <DashboardSkeleton />;
+};
+
+export default loading;
--- app/dashboard/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <p>Dashboard Page Chouaib</p>;
-}
