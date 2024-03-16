3d1f242ef9ddf6cdbaa564c6c71c7f319c80b994
Chouaib-Djerdi <djerdichouaib@gmail.com>
2024-03-16 13:48:05 +0100

    MetaData

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,6 +8,11 @@ import {
   RevenueChartSkeleton,
 } from '@/app/ui/skeletons';
 import CardWrapper from '@/app/ui/dashboard/cards';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default async function Page() {
   return (
